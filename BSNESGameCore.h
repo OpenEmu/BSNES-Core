e3aedac3d044174aadab51f71ae468e6e3bbd7a3
clobber <brymaster@gmail.com>
2013-09-02 16:50:32 -0700

    Fix license.

--- BSNESGameCore.h
+++ BSNESGameCore.h
@@ -1,6 +1,5 @@
 /*
  Copyright (c) 2009, OpenEmu Team
- 
 
  Redistribution and use in source and binary forms, with or without
  modification, are permitted provided that the following conditions are met:

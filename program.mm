f905e852e568eee3dbc683733823a2a48a14af17
Daniele Cattaneo <daniele.cattaneo@alice.it>
2020-01-06 19:32:49 +0100

    Update to BSNES 113.5

--- program.mm
+++ program.mm
@@ -54,6 +54,7 @@ struct Program : Emulator::Platform {
     auto openRomSuperFamicom(string name, vfs::file::mode mode) -> shared_pointer<vfs::file>;
     auto loadSuperFamicomFirmware(string fwname) -> void;
     
+    auto hackCompatibility() -> void;
     auto hackPatchMemory(vector<uint8_t>& data) -> void;
     
     auto updateVideoPalette() -> void;
@@ -143,37 +144,7 @@ auto Program::load() -> void
     emulator->unload();
     emulator->load();
 
-    // per-game hack overrides
-    auto title = superFamicom.title;
-    auto region = superFamicom.region;
-
-    //relies on mid-scanline rendering techniques
-    if(title == "AIR STRIKE PATROL" || title == "DESERT FIGHTER") emulator->configure("Hacks/PPU/Fast", false);
-
-    //stage 2 uses pseudo-hires in a way that's not compatible with the scanline-based renderer
-    if(title == "SFC クレヨンシンチャン") emulator->configure("Hacks/PPU/Fast", false);
-
-    //relies on cycle-accurate writes to the echo buffer
-    if(title == "KOUSHIEN_2") emulator->configure("Hacks/DSP/Fast", false);
-
-    //will hang immediately
-    if(title == "RENDERING RANGER R2") emulator->configure("Hacks/DSP/Fast", false);
-
-    //will hang sometimes in the "Bach in Time" stage
-    if(title == "BUBSY II" && region == "PAL") emulator->configure("Hacks/DSP/Fast", false);
-
-    //fixes an errant scanline on the title screen due to writing to PPU registers too late
-    if(title == "ADVENTURES OF FRANKEN" && region == "PAL") emulator->configure("Hacks/PPU/RenderCycle", 32);
-
-    //fixes an errant scanline on the title screen due to writing to PPU registers too late
-    if(title == "FIREPOWER 2000") emulator->configure("Hacks/PPU/RenderCycle", 32);
-
-    //fixes an errant scanline on the title screen due to writing to PPU registers too late
-    if(title == "NHL '94" || title == "NHL PROHOCKEY'94") emulator->configure("Hacks/PPU/RenderCycle", 32);
-
-    if (emulator->configuration("Hacks/Hotfixes")) {
-        if (title == "The Hurricanes") emulator->configure("Hacks/Entropy", "None");
-    }
+    hackCompatibility();
 
     emulator->power();
 }
@@ -433,6 +404,76 @@ auto Program::loadSuperFamicom(string location) -> bool
     return true;
 }
 
+// Keep in sync with bsnes/target-bsnes/program/hacks.cpp
+auto Program::hackCompatibility() -> void
+{
+    string entropy = ::SuperFamicom::configuration.hacks.entropy;
+    bool fastJoypadPolling = false;
+    bool fastPPU = ::SuperFamicom::configuration.hacks.ppu.fast;
+    bool fastPPUNoSpriteLimit = ::SuperFamicom::configuration.hacks.ppu.noSpriteLimit;
+    bool fastDSP = ::SuperFamicom::configuration.hacks.dsp.fast;
+    bool coprocessorDelayedSync = ::SuperFamicom::configuration.hacks.coprocessor.delayedSync;
+    uint renderCycle = 512;
+    
+    auto title = superFamicom.title;
+    auto region = superFamicom.region;
+    
+    if(title == "TAIKYOKU-IGO Goliath") fastJoypadPolling = true;
+    
+    //relies on mid-scanline rendering techniques
+    if(title == "AIR STRIKE PATROL" || title == "DESERT FIGHTER") fastPPU = false;
+    
+    //stage 2 uses pseudo-hires in a way that's not compatible with the scanline-based renderer
+    if(title == "SFC クレヨンシンチャン") fastPPU = false;
+    
+    //title screen game select (after choosing a game) changes OAM tiledata address mid-frame
+    //this is only supported by the cycle-based PPU renderer
+    if(title == "Winter olympics") fastPPU = false;
+    
+    //title screen shows remnants of the flag after choosing a language with the scanline-based renderer
+    if(title == "WORLD CUP STRIKER") fastPPU = false;
+    
+    //relies on cycle-accurate writes to the echo buffer
+    if(title == "KOUSHIEN_2") fastDSP = false;
+    
+    //will hang immediately
+    if(title == "RENDERING RANGER R2") fastDSP = false;
+    
+    //will hang sometimes in the "Bach in Time" stage
+    if(title == "BUBSY II" && region == "PAL") fastDSP = false;
+    
+    //fixes an errant scanline on the title screen due to writing to PPU registers too late
+    if(title == "ADVENTURES OF FRANKEN" && region == "PAL") renderCycle = 32;
+    
+    //fixes an errant scanline on the title screen due to writing to PPU registers too late
+    if(title == "FIREPOWER 2000" || title == "SUPER SWIV") renderCycle = 32;
+    
+    //fixes an errant scanline on the title screen due to writing to PPU registers too late
+    if(title == "NHL '94" || title == "NHL PROHOCKEY'94") renderCycle = 32;
+    
+    //fixes an errant scanline on the title screen due to writing to PPU registers too late
+    if(title == "Sugoro Quest++") renderCycle = 128;
+    
+    if(::SuperFamicom::configuration.hacks.hotfixes) {
+        //this game transfers uninitialized memory into video RAM: this can cause a row of invalid tiles
+        //to appear in the background of stage 12. this one is a bug in the original game, so only enable
+        //it if the hotfixes option has been enabled.
+        if(title == "The Hurricanes") entropy = "None";
+        
+        //Frisky Tom attract sequence sometimes hangs when WRAM is initialized to pseudo-random patterns
+        if(title == "ニチブツ・アーケード・クラシックス") entropy = "None";
+    }
+    
+    emulator->configure("Hacks/Entropy", entropy);
+    emulator->configure("Hacks/CPU/FastJoypadPolling", fastJoypadPolling);
+    emulator->configure("Hacks/PPU/Fast", fastPPU);
+    emulator->configure("Hacks/PPU/NoSpriteLimit", fastPPUNoSpriteLimit);
+    emulator->configure("Hacks/PPU/RenderCycle", renderCycle);
+    emulator->configure("Hacks/DSP/Fast", fastDSP);
+    emulator->configure("Hacks/Coprocessor/DelayedSync", coprocessorDelayedSync);
+}
+
+// Keep in sync with bsnes/target-bsnes/program/hacks.cpp
 auto Program::hackPatchMemory(vector<uint8_t>& data) -> void
 {
     auto title = superFamicom.title;
